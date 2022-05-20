f92aa18f1243272638497f85f6925e2fa636dc37
Mark <mark@marks-mac-mini.lan>
2022-05-20 10:38:11 -0400

    Coding challenge completed with Mocha tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
+
+/**
+ * Coding Challenge - needs much refactoring
+ * @param {*} str
+ * @returns 
+ */
 export function StackCalc(str) {
-    return 1;
+    let result = 0;
+    let parseArr = [];
+    let instructArr = str.split(" ");
+    let instructLen = instructArr.length;
+
+    instructArr.every((element, index) => {
+        if(!isNaN(element)){
+            parseArr.push(element);
+            return true;
+        }else{
+            if(element !== "DUP"){
+                let num1 = parseArr.pop();
+                let num2 = parseArr.pop();
+
+                result = performInstruction(num1, num2, element)
+
+                if(result === "error"){
+                    result = "Invalid instruction: " + element;
+                    return false;
+                }
+                if(index+1 < instructLen){
+                    parseArr.push(result)
+                }
+                
+            }else{
+                let _tempNum = parseArr.pop();
+                parseArr.push(_tempNum);
+                parseArr.push(_tempNum);
+            }
+            return true;
+        }
+    });
+    return result;
+}
+
+function performInstruction(num1, num2, item) {
+    let result;
+    let _n1 = parseInt(num1);
+    let _n2 = parseInt(num2);
+
+    switch (item){
+        case "+":
+            result = _n1 + _n2;
+            break;
+        case "-":
+            result = _n1 - _n2;
+            break;
+        case "*":
+            result = _n1 * _n2;
+            break;
+        case "/":
+            result = _n1 / _n2;
+            break;
+        default:
+            result = "error";
+    }
+    return result;
 }
--- test/test.js
+++ test/test.js
@@ -2,9 +2,34 @@ import assert from 'node:assert/strict';
 import { StackCalc } from '../src/index.js';
 
 describe('StackCalc', function () {
-  describe('when exercised it', function () {
-    it('should return 1', function () {
+  
+  describe('when exercised with ""', function () {
+    it('should return 0', function () {
+      assert.equal(StackCalc(""), 0);
+    });
+  });
+
+  describe('when exercised with "5 6 +"', function () {
+    it('should return 11', function () {
       assert.equal(StackCalc("5 6 +"), 11);
+    });
+  });
+
+  describe('when exercised with "3 DUP +"', function () {
+    it('should return 6', function () {
+      assert.equal(StackCalc("3 DUP +"), 6);
+    });
+  });
+
+  describe('when exercised with "6 5 5 7 * - /"', function () {
+    it('should return 5', function () {
+      assert.equal(StackCalc("6 5 5 7 * - /"), 5);
+    });
+  });
+
+  describe('when exercised with "x y +"', function () {
+    it('should return Invalid instruction', function () {
+      assert.equal(StackCalc("x y +"), "Invalid instruction: x");
     });
   });
 });
